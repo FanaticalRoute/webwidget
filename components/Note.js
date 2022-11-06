f9a4e2c149b8a15917f2ffe882804f435cddf830
Ewen Domeon <ewen.domeon@outlook.com>
2022-11-06 21:02:07 +0100

    note adding

--- components/Note.js
+++ components/Note.js
@@ -6,6 +6,21 @@ export default function Note() {
 
   return (
     <div className={css.notecontainer}>
+
+      <div id="notesInput" class={css.notesInput}>
+        <input id="titleInput" value="Title " autofocus />
+        <textarea id='noteInput'>Note</textarea>
+        <button id='btnDone'>Done</button>
+      </div>
+
+      <div id='display' class={css.notesOutput}>
+          <h1 class={css.h1text}>Your notes</h1>
+          <button id='btnAdd'>Add To Notes</button>
+        <div class={css.noteDisplayDiv}>
+        </div>
+
+      </div>
+
     </div>
     
   )
